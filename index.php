733d4b7ba8868a756b3d6fcfc0d5f3a4f269460b
Dmitriy Mamontov <mamontovdmitriy@mail.ru>
2017-05-21 11:06:36 +0600

    fix autoload

--- index.php
+++ index.php
@@ -6,7 +6,10 @@
  * Time: 19:23
  */
 
-spl_autoload_register();
+spl_autoload_register(function ($className) {
+    require_once (str_replace('\\', '/', $className) . ".php");
+    return false;
+});
 
 $config = require(__DIR__ . '/config.php');
 
